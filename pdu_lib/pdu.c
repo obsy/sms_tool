d2ef333593d9b947690e6f0b35c9faf812b19517
Cezary Jackiewicz <cezary@eko.one.pl>
2018-01-02 18:13:47 +0100

    Fix decoding sender name

--- pdu_lib/pdu.c
+++ pdu_lib/pdu.c
@@ -337,7 +337,7 @@ int pdu_decode(const unsigned char* buffer, int buffer_length,
 
 	const int sender_type_of_address = buffer[sms_deliver_start + 2];
 	if (sender_type_of_address == TYPE_OF_ADDRESS_ALPHANUMERIC) {
-		DecodePDUMessage_GSM_7bit(buffer + sms_deliver_start + 3, sender_number_length, output_sender_phone_number, sender_number_length);
+		DecodePDUMessage_GSM_7bit(buffer + sms_deliver_start + 3, (sender_number_length + 1) / 2, output_sender_phone_number, sender_number_length);
 	} else {
 		DecodePhoneNumber(buffer + sms_deliver_start + 3, sender_number_length, output_sender_phone_number);
 	}
